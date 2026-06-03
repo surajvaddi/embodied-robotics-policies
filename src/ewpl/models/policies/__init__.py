fc16c6877678bd9e36cd99823776c174a01b290c
Suraj Vaddi <surajnvaddi@gmail.com>
2026-06-03 18:26:33 -0400

    Fix optional torch policy export

--- src/ewpl/models/policies/__init__.py
+++ src/ewpl/models/policies/__init__.py
@@ -2,6 +2,13 @@
 
 from ewpl.models.policies.base import Policy, PolicyState
 from ewpl.models.policies.baselines import RandomPolicy, ReplayPolicy, make_policy
-from ewpl.models.policies.bc import BCPolicy
 
 __all__ = ["Policy", "PolicyState", "RandomPolicy", "ReplayPolicy", "BCPolicy", "make_policy"]
+
+
+def __getattr__(name: str):
+    if name == "BCPolicy":
+        from ewpl.models.policies.bc import BCPolicy
+
+        return BCPolicy
+    raise AttributeError(name)
